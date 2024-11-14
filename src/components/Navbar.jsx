b43f84ee75f5816e5287142bed518ba4a7ae035c
belaiily <ilyas.belaid@etu.univ-grenoble-alpes.fr>
2024-11-14 11:47:15 +0100

    ✨ Deployed NavBar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/kevinRushLogo.png";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-6 mb-20 bg">
+    <nav className="flex items-center justify-between py-6 mb-20 bg-blue-300 bg">
       <img src={logo} alt="logo" />
       <div className="flex items-center flex-shrink-0"></div>
     </nav>
