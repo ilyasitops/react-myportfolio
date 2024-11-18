474df68586a870ed1c3fe173239aa182879a596e
belaiily <ilyas.belaid@etu.univ-grenoble-alpes.fr>
2024-11-18 15:34:17 +0100

    💩 Navbar.jsx & App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="overflow-x-hidden antialiased text-neutral-300 slec selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="top-0 h-full flixed -z-10 w-fulll"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+      <div className="container px-8 mx-auto">
+        <Navbar />
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import logo from "../assets/kevinRushLogo.png";
-
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-6 mb-20 bg-blue-300 bg">
-      <img src={logo} alt="logo" />
-      <div className="flex items-center flex-shrink-0"></div>
+    <nav className="flex items-center justify-between py-6 mb-20">
+      <div className="flex items-center flex-shrink-0">
+        <img className="w-10 mx-2" src={logo} alt="logo" />
+      </div>
+      <div className="flex items-center justify-center gap-4 m-8 text-2xl">
+        <FaLinkedin className="text-white" />
+        <FaGithub className="text-white" />
+      </div>
     </nav>
   );
 };
